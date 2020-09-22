d8c9dc4b66eefff457d7193e18ffd43ba6f854b6
Matthes <matthes@leuffen.de>
2020-09-22 21:18:08 +0200

    update

--- doscan.sh
+++ doscan.sh
@@ -6,4 +6,5 @@ composer update
 
 vendor/bin/rudlvault inspect -o watchlist.unsealed.yml watchlist.yml
 ls -l
+
 docker run -t  -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/conf -v $(pwd)/docs:/out -e CONFIG_FILE=/conf/watchlist.unsealed.yml -e STATUS_FILE=/out/security.json nfra/secscanner
