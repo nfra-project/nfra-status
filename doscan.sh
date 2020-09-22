01b5202740edf55ea3425b024c989fb4be979e12
Matthes <matthes@leuffen.de>
2020-09-22 20:58:37 +0200

    update

--- doscan.sh
+++ doscan.sh
@@ -1,3 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 
+sudo apt-get install -y composer php-yaml
+composer update
+
+vendor/bin/rudlvault inspect watchlist.yml -o watchlist.unsealed.yml
+
+docker run -t  -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/conf -v $(pwd)/docs:/out -e CONFIG_FILE=/conf/watchlist.unsealed.yml -e STATUS_FILE=/out/security.json nfra/secscanner
